2d403d33bd52301b71f6af9d775cdb20f500216d
Drew Barrett <dfb1998@gmail.com>
2016-04-18 08:34:59 -0400

    highlighting

--- GridSquare.cpp
+++ GridSquare.cpp
@@ -15,7 +15,12 @@ GridSquare::GridSquare(int xPos, int yPos, int side)
     color = al_color_name("white");
 }
 
-void GridSquare::draw(ALLEGRO_MOUSE_STATE mouse)
+void GridSquare::draw(ALLEGRO_MOUSE_STATE state)
 {
+    if(state.x > x && state.x < x + sideLength && state.y > y && state.y < y + sideLength){
+        color = al_color_name("cyan");
+    }else{
+        color = al_color_name("white");
+    }
     al_draw_rectangle(x, y, x+sideLength, y+sideLength, color, 1);
 }
