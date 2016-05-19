ff6c872e046501402042a7932d401813966399df
Drew Barrett <dfb1998@gmail.com>
2016-05-19 16:39:23 -0400

    added null pointer check to prevent crash when enemies vector does not exist

--- Tower.cpp
+++ Tower.cpp
@@ -20,7 +20,7 @@ Tower::Tower(std::vector<Enemy>* memes, int xPos, int yPos)
 void Tower::Update()
 {
     //rotation++;
-    if((target == NULL || !target->isEnabled()) && enemies->size() > 0)
+    if((target == NULL || (enemies != NULL && (enemies->size() > 0 && !target->isEnabled()))))
     {
         target = &enemies->at(0);
     }
