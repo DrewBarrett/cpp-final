717551ef9c2d4651760b01b7a60f4daa2da2c112
Drew Barrett <dfb1998@gmail.com>
2016-05-19 20:24:41 -0400

    added half of the damn coh2 sountrack because git has a file limit... also support for sound fx

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "allegro5/allegro_primitives.h"
 #include "allegro5/allegro_font.h"
 #include "allegro5/allegro_ttf.h"
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #include "Button.h"
 #include "TowerMenu.h"
 #include "MapGrid.h"
@@ -15,99 +17,123 @@
 #define ScreenHeight 768
 int init();
 ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_SAMPLE *soundtrack = NULL;
 ALLEGRO_EVENT_QUEUE *queue;
 ALLEGRO_TIMER *timer;
 ALLEGRO_MOUSE_STATE state;
 
 int main(int argc, char **argv)
 {
-    init();
-    ALLEGRO_FONT *font = al_load_font("comic.ttf", 20, 0);
-    if (!font) {
-        fprintf(stderr, "Could not load 'comic.ttf'.\n");
-        return -1;
-    }
-    queue = al_create_event_queue();
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_mouse_event_source());
+	init();
+	ALLEGRO_FONT *font = al_load_font("comic.ttf", 20, 0);
+	if (!font) {
+		fprintf(stderr, "Could not load 'comic.ttf'.\n");
+		return -1;
+	}
+	queue = al_create_event_queue();
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_mouse_event_source());
 	timer = al_create_timer(1.0 / 60);
-    al_register_event_source(queue, al_get_timer_event_source(timer));
-    al_start_timer(timer);
+	al_register_event_source(queue, al_get_timer_event_source(timer));
+	al_start_timer(timer);
 
-    GUI gui = GUI();
+	GUI gui = GUI();
 
 
-    bool redraw = true;
-    bool title = true;
-    while (1) {
-        ALLEGRO_EVENT event;
-        al_wait_for_event(queue, &event);
-        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-            break;
-        if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
-            if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-                break;
-            if (title){
-                title = false;
-            }
-        }
-        if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			if(title){
-                title = false;
+	bool redraw = true;
+	bool title = true;
+	while (1) {
+		ALLEGRO_EVENT event;
+		al_wait_for_event(queue, &event);
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+			break;
+		if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+				break;
+			if (title) {
+				title = false;
+			}
+		}
+		if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			if (title) {
+				title = false;
 			}
 		}
 		if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
 			al_get_mouse_state(&state);
 			gui.MouseClicked();
 		}
-        if (event.type == ALLEGRO_EVENT_TIMER){
-            redraw = true;
-        }
-        if (redraw && al_is_event_queue_empty(queue)) {
-            redraw = false;
-            al_set_target_bitmap(al_get_backbuffer(display));
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            if(title){
-                al_clear_to_color(al_map_rgb(0, 0, 0));
-                al_draw_text(font, al_color_name("white"), ScreenWidth / 2, 0, ALLEGRO_ALIGN_CENTRE, "not Solitaire!");
-                //al_draw_bitmap(rules, (ScreenWidth / 2) - (al_get_bitmap_width(rules)/2) , 100, 0);
+		if (event.type == ALLEGRO_EVENT_TIMER) {
+			redraw = true;
+		}
+		if (redraw && al_is_event_queue_empty(queue)) {
+			redraw = false;
+			al_set_target_bitmap(al_get_backbuffer(display));
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			if (title) {
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_text(font, al_color_name("white"), ScreenWidth / 2, 0, ALLEGRO_ALIGN_CENTRE, "not Solitaire!");
+				//al_draw_bitmap(rules, (ScreenWidth / 2) - (al_get_bitmap_width(rules)/2) , 100, 0);
 				al_draw_text(font, al_color_name("white"), ScreenWidth / 2, ScreenHeight - 150, ALLEGRO_ALIGN_CENTRE,
 					"Press any key to continue or ESC to exit...");
 				al_draw_text(font, al_color_name("white"), ScreenWidth / 2, ScreenHeight - 100, ALLEGRO_ALIGN_CENTRE,
 					"Created by Drew Barrett");
-            }else{
-                al_get_mouse_state(&state);
-                gui.Draw(state);
-                //mapgrid.draw(state);
-            }
-            al_flip_display();
-        }
-    }
-    al_destroy_display(display);
-    return 0;
+			}
+			else {
+				al_get_mouse_state(&state);
+				gui.Draw(state);
+				//mapgrid.draw(state);
+			}
+			al_flip_display();
+		}
+	}
+	al_destroy_display(display);
+	return 0;
 }
 
-int init(){
+int init() {
 
-    if(!al_init())
-    {
-        fprintf(stderr, "failed to initialize allegro!\n");
-        return -1;
-    }
-    display = al_create_display(ScreenWidth, ScreenHeight);
-    if(!display)
-    {
-        fprintf(stderr, "failed to create display!\n");
-        return -1;
-    }
-    al_clear_to_color(al_map_rgb(0,0,0));
-    al_init_image_addon();
-    if(!al_init_primitives_addon()){
-        fprintf(stderr, "failed to initialize primitives!\n");
-        return -1;
-    }
-    al_init_font_addon();
-    al_init_ttf_addon();
-    al_install_mouse();
-    al_install_keyboard();
+	if (!al_init())
+	{
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+	display = al_create_display(ScreenWidth, ScreenHeight);
+	if (!display)
+	{
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_init_image_addon();
+	if (!al_init_primitives_addon()) {
+		fprintf(stderr, "failed to initialize primitives!\n");
+		return -1;
+	}
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_install_mouse();
+	al_install_keyboard();
+	if (!al_install_audio()) {
+		fprintf(stderr, "failed to initialize audio!\n");
+		return -1;
+	}
+
+	if (!al_init_acodec_addon()) {
+		fprintf(stderr, "failed to initialize audio codecs!\n");
+		return -1;
+	}
+	//change this for amount of simultanious noises playing
+	if (!al_reserve_samples(2)) {
+		fprintf(stderr, "failed to reserve samples!\n");
+		return -1;
+	}
+
+	soundtrack = al_load_sample("soundtrack.ogg");
+
+	if (!soundtrack) {
+		fprintf(stderr, "Could not load sample from soundtrack.mp3!\n");
+		return -1;
+	}
+	al_play_sample(soundtrack, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 }
